fc4d0c67fb24bfd4b48d7d774fb378c79158a0cc
Evan Blaudy <evan@cg-wire.com>
2022-10-04 03:12:30 +0200

    [qa] get version of the module directly in the __version__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
+from distutils.util import convert_path
 
-setup()
+# Get version without sourcing dccutils module
+# (to avoid importing dependencies yet to be installed)
+main_ns = {}
+with open(convert_path("dccutils_server/__version__.py")) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+
+setup(version=main_ns["__version__"])
