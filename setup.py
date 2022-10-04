8b8f1b37ac6fce4d8c41a20f31f6de08e19457f7
Evan Blaudy <evan@cg-wire.com>
2022-10-04 03:26:25 +0200

    [qa] Fix dccutils/__version.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.util import convert_path
 # Get version without sourcing dccutils module
 # (to avoid importing dependencies yet to be installed)
 main_ns = {}
-with open(convert_path("dccutils_server/__version__.py")) as ver_file:
+with open(convert_path("dccutils/__version__.py")) as ver_file:
     exec(ver_file.read(), main_ns)
 
 
